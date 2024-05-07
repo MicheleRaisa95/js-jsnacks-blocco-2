bca4ea0987f6cc3827d666ba47eeef776f93bf5e
MicheleRaisa95 <micheleraisa95@gmail.com>
2024-05-07 10:55:52 +0200

    aggiunte correzioni

--- snack-4/js/main.js
+++ snack-4/js/main.js
@@ -8,23 +8,26 @@ const min = [7, 8, 9];
 
 // trova array piu corto
 
-const minLunghezzaArray = Math.min(max.length, min.length);
+//const minLunghezzaArray = Math.min(max.length, min.length);
 
 //console.log(Math.min(max.length, min.length));
 
-// Aggiungi elementi casuali all’array che ha meno elementi
+
+
+//fai si che alla fine i due array abbiano gli stessi elementi
 
 while (max.length !== min.length) {
+
     if (max.length < min.length) {
-        // Aggiungi un elemento casuale 
+        // Aggiungi elementi casuali all’array che ha meno elementi
+        
         max.push(Math.floor(Math.random() * 10));
     } else {
-        // Aggiungi un elemento casuale 
+        
         min.push(Math.floor(Math.random() * 10));
     }
 }
 
-// fino a quando ne avrà tanti quanti l’altro
 
 console.log("Array 1:",max);
 console.log("Array 2:",min);
