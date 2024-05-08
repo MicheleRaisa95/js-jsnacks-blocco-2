21db9192afb2bbcd5d0b3c2b9f787c933ca47916
MicheleRaisa95 <micheleraisa95@gmail.com>
2024-05-08 18:55:36 +0200

    esecuzione snack-c

--- snack-c/js/main.js
+++ snack-c/js/main.js
@@ -1,1 +1,18 @@
 'use strict'
+
+const array = [30, 60, 90, 56]
+
+function stampa(array) {
+    let result = '';
+for (let i = 0; i < array.length; i++)  {
+    result += array[i];
+    if (i !== array.length - 1) {
+result += ',';
+    }
+}
+return result;
+}
+
+const stampaArray = stampa(array);
+
+console.log(stampaArray);
